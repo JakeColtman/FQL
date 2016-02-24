e137525fd58addd413aa5b4a4baf013561c8c373
Journeyman08 <jakecoltman@sky.com>
2016-02-24 19:22:29 +0000

    Added working implementation
    Demonstration purposes

--- process.py
+++ process.py
@@ -1,3 +1,10 @@
+from Repository import Repository
+from Parser import Parser
+
 def process(repo, parser, queryList):
     for query in queryList:
         repo.add_queries(parser.parse(query))
+
+repo = Repository()
+process(repo, Parser(), ["with test_cte as (SELECT 1),test_cte2 as (SELECT 3 )  Select 2"])
+print(repo.queries)
