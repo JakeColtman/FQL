2813bb6d09e6c4d1aaf3fd08f9276e32f135a31c
Journeyman08 <jakecoltman@sky.com>
2016-02-24 19:17:51 +0000

    Updated repo to take many queries
    Enables repo to take as an input what parse gives as an output and thus
    avoiding code messiness

--- Repository.py
+++ Repository.py
@@ -6,3 +6,6 @@ class Repository:
     def add_query(self, query):
         self.queries.append(query)
         return True
+
+    def add_queries(self, queries):
+        return all([self.add_query(x) for x in queries])
