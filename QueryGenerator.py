167e410fdf3c6964881cd3e35d6206faab869aae
jakecoltman <jakecoltman@sky.com>
2016-02-25 18:40:50 +0000

    Added query generator to sit over repository
    Purpose is to recursively find dependencies and add them to the query

---
+++ QueryGenerator.py
@@ -0,0 +1,14 @@
+from Repository import Repository
+
+class QueryGenerator:
+
+    def __init__(self, repository):
+        self.repo = repository
+
+    def _get_relevant_queries_from_repo(self, query_names):
+        output = []
+        return output
+
+    def generate_query_with(self, query_names):
+        return ""
+
---
+++ Tests/QueryGeneratorTests.py
@@ -0,0 +1,21 @@
+import unittest
+from Repository import Repository
+from Query import Query
+from QueryGenerator import QueryGenerator
+
+class QueryGeneratorTests(unittest.TestCase):
+
+    def test_query_generator_finds_first_level_queries(self):
+        self.assertEqual(True, False)
+        q1 = Query("1", "", [])
+        q2 = Query("2", "", [])
+        q3 = Query("3", "", [])
+        repo = Repository()
+        repo.add_queries([q1,q2,q3])
+        qg = QueryGenerator(repo)
+        output = qg._get_relevant_queries_from_repo([q1,q1])
+        self.assertTrue(q1 in output)
+        self.assertTrue(q2 in output)
+
+if __name__ == '__main__':
+    unittest.main()
