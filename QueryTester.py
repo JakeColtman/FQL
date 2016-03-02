23a20868c150ca79f844ca5cdad0aab42812c106
jakecoltman <jakecoltman@sky.com>
2016-03-02 18:07:38 +0000

    Simple test object
    Down the line consider making the test run itself?

--- QueryTester.py
+++ QueryTester.py
@@ -1,3 +1,12 @@
 from collections import namedtuple
 
 Test = namedtuple("QueryTester", ["queryName", "tableLookup", "outputTable"])
+
+class Tester:
+
+    def __init__(self, connection, query_generator):
+        self.connection = connection
+        self.query_generator = query_generator
+
+    def run_test(self, test : Test, verbose = True):
+        return False
