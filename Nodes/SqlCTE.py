9eb50a6f8d395f56da8f690dc80d064097c586df
jakecoltman <jakecoltman@sky.com>
2016-03-15 18:44:51 +0000

    Added shells for different types of nodes
    Each will represent a different kind of thing in the query graphs

---
+++ Nodes/SqlCTE.py
@@ -0,0 +1,20 @@
+
+class SqlTableNode:
+
+    def get_columns(self):
+        return None
+
+    def get_dependencies(self):
+        return None
+
+    def get_docstring(self):
+        return None
+
+    def set_columns(self):
+        return None
+
+    def set_dependencies(self):
+        return None
+
+    def set_docstring(self):
+        return None
