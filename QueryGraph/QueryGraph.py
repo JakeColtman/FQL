372a230b9cfe5c88362c7c1cef20e7073a16abfc
jakecoltman <jakecoltman@sky.com>
2016-03-16 18:01:17 +0000

    Added serializer
    Turns the query graph into a sql query - directory allows for other
    types of serializers down the line

--- QueryGraph/QueryGraph.py
+++ QueryGraph/QueryGraph.py
@@ -25,3 +25,13 @@ class QueryGraph:
         for node in query_graph.node_lookup:
             if type(node) != PlaceholderNode:
                 self.node_lookup[node.node.get_name()] = node
+
+    def get_ordered_node_list_from_node(self, node: Node):
+        output = [node]
+        depList = node.get_dependencies()
+        while len(depList) != 0:
+            [output.append(x) for x in depList]
+            dependentNodes = [x.get_dependencies() for x in depList]
+            depList = [item for sublist in dependentNodes for item in sublist]
+        output.reverse()
+        return output
---
+++ Serializer/SQLSerializer.py
@@ -0,0 +1,23 @@
+from QueryGraph.QueryGraph import QueryGraph
+from Nodes.Node import Node
+
+class SqlSerializer:
+
+    def __init__(self, graph: QueryGraph):
+
+        self.graph = graph
+
+    def serialize_to_node(self, node: Node):
+
+        nodeList = self.graph.get_ordered_node_list_from_node(node)
+
+        if len(nodeList) == 1:
+            return nodeList[0].get_text()
+
+        output = "with "
+        for item in nodeList[:-1]:
+            output += "{0} as ({1}),".format(item.get_name(), item.get_text()) + "\n"
+        output = output[:-2]
+        output += "\n"
+        output += nodeList[-1].text
+        return output
